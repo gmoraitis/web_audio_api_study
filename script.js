16d2bbde9923705f80eae74a4b357969a3eac016
George <moraitis.georgios@icloud.com>
2022-09-15 13:09:53 +0300

    Added working initial script

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-var contextClass = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
-if (contextClass) {
-    // Web Audio API is available. 
-    var context = new contextClass();
-} else {
-    // Web Audio API is not available. Ask the user to use a supported browser.
-}
+// var contextClass = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
+// if (contextClass) {
+//     // Web Audio API is available. 
+//     var context = new contextClass();
+// } else {
+//     // Web Audio API is not available. Ask the user to use a supported browser.
+// }
 
 
-document.getElementById('start').addEventListener('click', function () {
+// document.getElementById('start').addEventListener('click', function () {
 
-    // Setup all nodes
-    let osc = context.createOscillator();
-    osc.type = "sine";
-    osc.connect(context.destination);
-    osc.start(context.currentTime);
-});
+//     // Setup all nodes
+//     let osc = context.createOscillator();
+//     osc.type = "sine";
+//     osc.connect(context.destination);
+//     osc.start(context.currentTime);
+// });
 
 // Make the stop button
+
+const audioContext = new AudioContext(); //___Initializes web audio api
+let osc;
+
+function playOsc(oscType, freq) {
+    if (osc) {
+        osc.stop(audioContext.currentTime); 
+        console.log("Stopped");
+        osc = undefined;
+    } else {
+        osc = audioContext.createOscillator();
+        osc.type = oscType;
+        osc.frequency.value = freq; 
+        osc.connect(audioContext.destination); 
+        osc.start(audioContext.currentTime); 
+        console.log("Start")
+    }
+}
+document.getElementById('start').addEventListener("click", function () {
+    playOsc("sine", 330);
+});
+
